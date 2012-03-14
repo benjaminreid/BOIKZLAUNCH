ebde5456929d7e04f4cef4bba79dcdc1a9c5f7bd
Benjamin Reid <benjamin@nouveller.com>
2012-03-14 22:20:03 +0000

    Smooth out the parallax layer

--- js/global.js
+++ js/global.js
@@ -10,19 +10,7 @@
 
   $(window).on('mousemove', function(e) {
     // store the mouse x and y
-    mouseX = e.pageX, mouseY = e.pageY,
-    // calculate the offset from the centre
-    xFromCentre = mouseX - screenW/2,
-    // calculate the percentage from the centre
-    percentFromCentre = (xFromCentre / ((screenW/2) / 100)),
-    // calculate the pixels to move
-    pxToMove = Math.round((amountToMove/100 * percentFromCentre)),
-    // inverse the movement
-    pxToMove = pxToMove - (pxToMove*2);
-
-    layer.css({
-      left: pxToMove
-    });
+    mouseX = e.pageX, mouseY = e.pageY;
   });
 
   $(window).on('resize', function() {
@@ -34,8 +22,27 @@
     return screenW;
   }
 
+  function updateParallaxLayer()
+  {
+    setInterval(function() {
+      // calculate the offset from the centre
+      xFromCentre = mouseX - screenW/2,
+      // calculate the percentage from the centre
+      percentFromCentre = (xFromCentre / ((screenW/2) / 100)),
+      // calculate the pixels to move
+      pxToMove = (amountToMove/100 * percentFromCentre),
+      // inverse the movement
+      pxToMove = pxToMove - (pxToMove*2);
+
+      layer.css({
+        left: pxToMove
+      });
+    }, 1000/60);
+  }
+
   function init() {
     updateScreen();
+    updateParallaxLayer();
   }
   init();
 
